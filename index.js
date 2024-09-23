9482236ce05a8b1b7d30bdf165cc18c6768fab25
IgniteProjects <paucarjason2@gmail.com>
2024-09-22 22:07:26 -0400

    upload

---
+++ index.js
@@ -0,0 +1,18 @@
+const doc = document;
+const menuOpen = doc.querySelector(".menu");
+const menuClose = doc.querySelector(".close");
+const overlay = doc.querySelector(".overlay");
+
+menuOpen.addEventListener("click", () => {
+  overlay.classList.add("overlay--active");
+});
+
+menuClose.addEventListener("click", () => {
+  overlay.classList.remove("overlay--active");
+});
+
+function handleScroll() {
+  document.getElementById('content-container').scrollIntoView({ behavior: 'smooth' });
+}
+
+
